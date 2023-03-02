ed64cbf923a38b65f99561a334bee1433aac5f98
Ash <96686884+ashleygailhilburn@users.noreply.github.com>
2023-03-02 11:20:18 -0600

    building up some data
    This may need to be moved into a class to make an array, object, SOMETHING else, I just don't know enough yet for that...

--- main/java/org/example/Main.java
+++ main/java/org/example/Main.java
@@ -3,9 +3,25 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-    String[] strArray1 = new String[] {"Monday", "Tuesady", "Wednesday", "Thursday", "Friday"};
+    String[] weekdays = new String[] {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+    /*System.out.println(strArray1[0]);*/
+    for ( int i = 0; i < weekdays.length; i++) {
+        /*System.out.println(weekdays[i]);*/
+    }
 
-    System.out.println(strArray1[2]);
+    String[] weeklyChores = {"Laundry", "Mopping", "De-Clutter Box", "Groceries", "Cooking", "Dusting", "Bathrooms", "Put Away Rooms", "Process Papers"};
+    /*System.out.println(weeklyChores[5]);*/
+        System.out.println("Your Weekly Chores are: ");
+    for ( int i = 0; i < weeklyChores.length; i++) {
+        System.out.println(weeklyChores[i]);
+    }
+        System.out.println("\nYour Daily Chores are: ");
+    String[] dailyChores = {"Dishes", "Box all Clutter", "Toys", "Sweeping", "Trash"};
+    /*System.out.println(dailyChores[3]);*/
+    for ( int i = 0; i < dailyChores.length; i++) {
+        System.out.println(dailyChores[i]);
+    }
+
 
     }
 }
